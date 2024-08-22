10346d9354a6dceeb54ab3bae7206cfa0cb2fbfa
sebastianterleira <seb.terleira1204@gmail.com>
2024-08-22 15:34:49 -0600

    chore: agregue algunos codigo de prueba

---
+++ src/utils/codes.js
@@ -0,0 +1,8 @@
+export const codes = {
+  GRAVITY: "Happy Birthday! You're just as mysterious as this code!",
+  HAPPYBIRTHDAY: "Another year, another mystery solved. Or is it?",
+  SECRET32: "The answer to life, the universe, and... your birthday!",
+  YOYO: "Una criatura muy hermosa en muchas dimensiones, bill tiene una!.",
+  MYSTERY: "Wow lo lograste, es solo el comienzo"
+  // Puedes agregar más códigos aquí.
+};
