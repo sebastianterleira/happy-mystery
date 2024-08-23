7d35f82cafa3c72ef64119ad2f65417a1eb6c49e
sebastianterleira <seb.terleira1204@gmail.com>
2024-08-22 20:37:26 -0600

    feat: update code.js

--- src/utils/codes.js
+++ src/utils/codes.js
@@ -3,6 +3,14 @@ export const codes = {
   HAPPYBIRTHDAY: "Another year, another mystery solved. Or is it?",
   SECRET32: "The answer to life, the universe, and... your birthday!",
   YOYO: "Una criatura muy hermosa en muchas dimensiones, bill tiene una!.",
-  MYSTERY: "Wow lo lograste, es solo el comienzo"
+  MYSTERY: "Wow lo lograste, es solo el comienzo",
+  BILL: "Asi que quieres saber mas sobre mi...",
+  BILLCIPHER: "Soy perfectamente geometrico, pero no me importa.",
+  DORITO: "No debiste decirme eso.",
+  ANSWER: "QUESTION",
+  ASSHOLE: 'NOT S&P approved, wash your mouth out with soap!',
+  CUM: "NOT S&P approved, wash your mouth out with soap!",
+  AXOLOTL: "HACES MUCHAS PREGUNTAS...",
+  BABY: "FELICIDADES, ADIVINA QUÉ ESTÁ CRECIENDO DENTRO DE TI AHORA MISMO. ¡NOS VEMOS EN 9 MESES, PAPÁ!"
   // Puedes agregar más códigos aquí.
 };
