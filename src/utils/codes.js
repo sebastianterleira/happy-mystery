cbf8d488ed5c9cdd4daf0430601668ad8053d6b2
sebastianterleira <seb.terleira1204@gmail.com>
2024-08-23 18:36:11 -0600

    add: new code muerte?

--- src/utils/codes.js
+++ src/utils/codes.js
@@ -2,15 +2,20 @@ export const codes = {
   GRAVITY: "Happy Birthday! You're just as mysterious as this code!",
   HAPPYBIRTHDAY: "Another year, another mystery solved. Or is it?",
   SECRET32: "The answer to life, the universe, and... your birthday!",
-  YOYO: "Una criatura muy hermosa en muchas dimensiones, bill tiene una!.",
+  "+D1": "#t3441f@fla0",
+  YOYO: "Una criatura muy hermosa en muchas dimensiones multiversales, bill tiene una!.",
   MYSTERY: "Wow lo lograste, es solo el comienzo",
   BILL: "Asi que quieres saber mas sobre mi...",
-  BILLCIPHER: "Soy perfectamente geometrico, pero no me importa.",
+  BILLCIPHER: "Soy una idea perfectamente geometrica.",
   DORITO: "No debiste decirme eso.",
+  "MUERTE?": "¿Que es eso?",
   ANSWER: "QUESTION",
   ASSHOLE: 'NOT S&P approved, wash your mouth out with soap!',
   CUM: "NOT S&P approved, wash your mouth out with soap!",
   AXOLOTL: "HACES MUCHAS PREGUNTAS...",
-  BABY: "FELICIDADES, ADIVINA QUÉ ESTÁ CRECIENDO DENTRO DE TI AHORA MISMO. ¡NOS VEMOS EN 9 MESES, PAPÁ!"
+  BABY: "FELICIDADES, ADIVINA QUÉ ESTÁ CRECIENDO DENTRO DE TI AHORA MISMO. ¡NOS VEMOS EN 9 MESES, PAPÁ!",
+  HL1: "Cómo era?, Gordon que?",
+  HL2: "Lamar!!, donde estas!?",
+  HL3: "Bill tambien perdio la esperanza de algún día ver ese 3..."
   // Puedes agregar más códigos aquí.
 };
